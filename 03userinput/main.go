cf1a61abf4ab39ae80a3c864e58fb156e2325010
Ritesh Karankal <ritesh.karankal200@gmail.com>
2023-08-25 01:13:34 +0530

    user input

---
+++ 03userinput/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	welcome := "Welcome to user input"
+	fmt.Println(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Rating:")
+
+	// comma ok || err ok
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating, ", input)
+	fmt.Printf("The type is %T", input)
+
+	// fmt.Println(err)
+
+}
